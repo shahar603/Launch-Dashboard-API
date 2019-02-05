0239351e46e56e6e3a39e3d7ef32f7d692437e78
Shahar Satamker <shahar.s603@gmail.com>
2019-02-05 11:24:53 +0200

    Created an empty test file for future tests of the API

---
+++ test/routes/launches_test.js
@@ -0,0 +1,15 @@
+// Import assert to unit test the database
+const assert = require("assert");
+
+
+
+// Create a series of tests
+describe("Testing the API is returning the correct data", function(){
+
+
+    it("", function(done){
+        
+    });
+
+
+});
