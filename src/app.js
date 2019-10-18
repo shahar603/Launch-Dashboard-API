ad46a132d49d8b9d634cd49a7c52b6bbc6decf4c
Shahar Satamker <27346413+shahar603@users.noreply.github.com>
2019-10-18 14:12:04 +0300

    Added request datetime to logger

--- src/app.js
+++ src/app.js
@@ -97,8 +97,19 @@ app.use(function(req, res, next) {
 });
 
 
+
+
 // Logging the request (without identifing detaills);
-app.use(morgan("tiny"));
+app.use(morgan(function (tokens, req, res) {
+    return [
+        "[", tokens.date(req, res), "]",
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, "content-length"), "-",
+        tokens["response-time"](req, res), "ms"
+    ].join(" ");
+  }));
 
 // ##################### ROUTES #####################
 
